f93c02e22082816f5f9526a85ae7fc6c538fff46
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2024-06-01 18:11:03 -0300

    favicon

--- components/StudiesComponent/StudiesComponent.tsx
+++ components/StudiesComponent/StudiesComponent.tsx
@@ -11,7 +11,13 @@ export default function StudiesComponent() {
                     <p className="subtitle">Desarrollador Full Stack, mar. 2022 — ago. 2024</p>
                 </div>
                 <p>
-                    Con más de 700 horas de práctica donde aprendí múltiples tecnologías, aplicación real en proyectos, desarrollo de un proyecto grupal con modalidad SCRUM para realizar un ecommerce y un proyecto individual consistente en un SPA.
+                    Con más de 700 horas de práctica donde aprendí múltiples tecnologías, aplicación real en proyectos, desarrollo de un proyecto grupal con modalidad SCRUM para realizar un ecommerce y un proyecto individual consistente en un SPA. <Link href={"https://certificates.soyhenry.com/cert?id=ab1a5fb5-9b2e-454b-a8fb-5e4d3ae64eb1"}
+                        aria-label="https://certificates.soyhenry.com/cert?id=ab1a5fb5-9b2e-454b-a8fb-5e4d3ae64eb1"
+                        target="_blank" rel="noopener noreferrer"
+                        className="link"
+                    >
+                        Certificado
+                    </Link>
                 </p>
                 <ul className="list-items-container">
                     <li className="list-item">
@@ -21,15 +27,6 @@ export default function StudiesComponent() {
                         <strong>Proyecto Grupal:</strong> Es una aplicación web que ha sido pensada para la compra y venta de productos de computación. Cuenta con Carrito de compras, Custom PC Builder, Wish List, filtrados y paginado de productos. Tambien cuenta con la implementación de mercado pago, registro de usuarios y distintos roles para su navegación.
                     </li>
                 </ul>
-                <p>
-                    <Link href={"https://certificates.soyhenry.com/cert?id=ab1a5fb5-9b2e-454b-a8fb-5e4d3ae64eb1"}
-                        aria-label="https://certificates.soyhenry.com/cert?id=ab1a5fb5-9b2e-454b-a8fb-5e4d3ae64eb1"
-                        target="_blank" rel="noopener noreferrer"
-                        className="link"
-                    >
-                        Certificado
-                    </Link>
-                </p>
             </div>
         </div>
     )
